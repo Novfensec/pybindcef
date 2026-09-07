37f519ed7cf24d8b5790eebf0a83f36a51cab3fc
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-09-07 12:31:54 +0530

    update

--- cef_worker/platforms/win/cef_worker.cpp
+++ cef_worker/platforms/win/cef_worker.cpp
@@ -1,7 +1,8 @@
 #include "include/capi/cef_app_capi.h"
 #include <windows.h>
-int main(int argc, char** argv) {
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
     cef_main_args_t args;
-    args.instance = GetModuleHandle(nullptr); 
+    args.instance = hInstance;
     return cef_execute_process(&args, nullptr, nullptr);
 }
