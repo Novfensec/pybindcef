4f0d4358dc6a9e31c997799d26674fd953f3496e
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-09-12 17:02:47 +0530

    ld library for cef_worker

--- src/pybindcef/__init__.py
+++ src/pybindcef/__init__.py
@@ -3,15 +3,15 @@ import platform
 import sys
 
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
-SHARE_DIR = os.path.join(sys.prefix, "share", "pybindcef")
 
 if os.name == "nt":
+    SHARE_DIR = os.path.join(sys.prefix, "share", "pybindcef")
     if hasattr(os, "add_dll_directory"):
         os.add_dll_directory(SHARE_DIR)
+    os.environ["PATH"] = f"{SHARE_DIR};{PACKAGE_DIR};{os.environ.get('PATH', '')}"
 else:
+    SHARE_DIR = os.path.join(os.path.dirname(PACKAGE_DIR), "pybindcef.libs")
     os.environ["LD_LIBRARY_PATH"] = f"{SHARE_DIR}:{os.environ.get('LD_LIBRARY_PATH', '')}"
-
-os.environ["PATH"] = f"{SHARE_DIR};{PACKAGE_DIR};{os.environ.get('PATH', '')}"
 
 from ._pybindcef import * # type: ignore
 
